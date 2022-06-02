b2e231cc9b16a8b4e37235f27b9b8c4823248275
solidfry <ellioteweb@gmail.com>
2022-06-02 22:20:45 +1000

    added inputs and dummy controls fixed issue with omnisharp

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,23 +5,37 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
     public Inputs controls;
+    public GameObject dropper;
+    private bool isDropped = false;
+
     private void Awake()
     {
-        controls.PlayerControls.Movement.performed += _ => Drop();
-    }
-    
-    void Drop() 
-    {
-        Debug.Log("Dropped");
+        controls = new Inputs();
+        controls.PlayerControls.Drop.performed += context => Drop();
+        controls.PlayerControls.Movement.performed += context => Aim();
     }
 
-    void OnEnable()
+    void Drop()
+    {
+        Debug.Log("Dropped");
+        if (dropper != null && isDropped == false)
+            dropper.GetComponent<Rigidbody2D>().simulated = true;
+    }
+
+    void Aim()
+    {
+        if (isDropped == false)
+            Debug.Log("Aimed");
+    }
+
+    private void OnEnable()
     {
         controls.Enable();
     }
-    
-    void OnDisable()
+
+    private void OnDisable()
     {
         controls.Disable();
     }
